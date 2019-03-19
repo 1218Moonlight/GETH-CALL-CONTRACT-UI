7fd7076d8cb675d90559a55501d0e60fac9e8563
kmoon <1218moonlight@gmail.com>
2019-03-19 19:15:50 +0900

    GUI관련 소스 추가. GUI関連のコードを追加。

---
+++ GUI/gui_test.go
@@ -0,0 +1,9 @@
+package GUI
+
+import (
+	"testing"
+)
+
+func TestGui(t *testing.T) {
+	main()
+}
---
+++ GUI/main.go
@@ -0,0 +1,49 @@
+package GUI
+
+import (
+	"github.com/andlabs/ui"
+	_ "github.com/andlabs/ui/winmanifest"
+	"log"
+)
+
+type window struct {
+	main       *ui.Window
+	title      string
+	width      int
+	height     int
+	hasMenubar bool
+}
+
+func newWindow() window {
+	return window{
+		main:       nil,
+		title:      "GETH-CALL-CONTRACT-UI",
+		width:      600,
+		height:     600,
+		hasMenubar: false,
+	}
+}
+
+func main() {
+	ui.Main(gui)
+}
+
+func gui(){
+	win := newWindow()
+
+	win.main = ui.NewWindow(win.title, win.width, win.height, win.hasMenubar)
+
+	win.main.OnClosing(func(window *ui.Window) bool {
+		ui.Quit()
+		log.Println("OnClosing")
+		return true
+	})
+
+	ui.OnShouldQuit(func() bool {
+		win.main.Destroy()
+		log.Println("OnShouldQuit")
+		return true
+	})
+
+	win.main.Show()
+}
