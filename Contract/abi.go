e2952c1b9b984af485623fa144b93a163cc56689
kmoon <1218moonlight@gmail.com>
2019-03-21 11:12:18 +0900

    [ contract ] call 문제 해결중... call問題を解決中。。。

--- Contract/abi.go
+++ Contract/abi.go
@@ -11,10 +11,10 @@ type (
 )
 
 const (
-	balanceAbi  = `[{"constant":true,"inputs":[{"name": "_owner","type": "address"}],"name": "balanceOf", "outputs": [{"name": "balance","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"}]`
-	symbolAbi   = `[{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name": "","type": "string"}],"payable":false,"stateMutability":"view","type":"function"}]`
 	nameAbi     = `{"constant": true,"inputs": [],"name": "name","outputs": [{"name": "","type": "string"}],"payable": false,"stateMutability": "view","type": "function"}`
 	totalSupply = `{"constant": true,"inputs": [],"name": "totalSupply","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"}`
+	balanceAbi  = `[{"constant":true,"inputs":[{"name": "_owner","type": "address"}],"name": "balanceOf", "outputs": [{"name": "balance","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"}]`
+	symbolAbi   = `[{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name": "","type": "string"}],"payable":false,"stateMutability":"view","type":"function"}]`
 	decimals    = `{"constant": true,"inputs": [],"name": "decimals","outputs": [{"name": "","type": "uint8"}],"payable": false,"stateMutability": "view","type": "function"}`
 	allowance   = `{"constant": true,"inputs": [{"name": "_owner","type": "address"},{"name": "_spender","type": "address"}],"name": "allowance","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"}`
 )
@@ -23,9 +23,9 @@ func GetAbi(stringAbi string) (abiABI, error) {
 	return abi.JSON(strings.NewReader(stringAbi))
 }
 
-func GetBalanceOfAbi() (*big.Int, abiABI, error) {
+func GetBalanceOfAbi() (**big.Int, abiABI, error) {
 	bAbi, err := GetAbi(balanceAbi)
-	return new(big.Int), bAbi, err
+	return new(*big.Int), bAbi, err
 }
 
 func GetSymbolAbi() (*string, abiABI, error) {
@@ -38,17 +38,17 @@ func GetNameAbi() (*string, abiABI, error) {
 	return new(string), nAbi, err
 }
 
-func GetTotalSupplyAbi() (*big.Int, abiABI, error) {
+func GetTotalSupplyAbi() (**big.Int, abiABI, error) {
 	tAbi, err := GetAbi(totalSupply)
-	return new(big.Int), tAbi, err
+	return new(*big.Int), tAbi, err
 }
 
-func GetDecimalsAbi() (*big.Int, abiABI, error) {
+func GetDecimalsAbi() (**big.Int, abiABI, error) {
 	dAbi, err := GetAbi(decimals)
-	return new(big.Int), dAbi, err
+	return new(*big.Int), dAbi, err
 }
 
-func GetAllowance() (*big.Int, abiABI, error) {
+func GetAllowance() (**big.Int, abiABI, error) {
 	aAbi, err := GetAbi(allowance)
-	return new(big.Int), aAbi, err
+	return new(*big.Int), aAbi, err
 }
