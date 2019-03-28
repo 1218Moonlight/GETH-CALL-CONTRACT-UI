d1a8cd72cc5b602e44b26a5085279871e1c6a681
kmoon <1218moonlight@gmail.com>
2019-03-28 15:22:05 +0900

    main ui 기본작업 완료. main ui 基本的な作業完了。

--- src/GUI/uis.go
+++ src/GUI/uis.go
@@ -2,17 +2,49 @@ package GUI
 
 import "github.com/andlabs/ui"
 
+type uiBox struct {
+	mainVerticalBox   *ui.Box
+}
+
 func windowUI() {
 	win := newWindow()
 	win.onClosingAndQuit()
 
-	topVbox := ui.NewVerticalBox()
-	topVbox.SetPadded(true)
+	boxManager := uiBox{mainVerticalBox:InitMainVerticalBox()}
 
-	ipBtn := ui.NewButton("testBtn")
-	topVbox.Append(ipBtn, true)
+	boxManager.Entry("CA")
+	boxManager.Entry("EOA")
+	boxManager.Erc20Info()
 
-	win.setChild(topVbox)
+	win.setChild(boxManager.mainVerticalBox)
 
 	win.show()
+}
+
+func InitMainVerticalBox()*ui.Box{
+	box := ui.NewVerticalBox()
+	box.SetPadded(true)
+	return box
+}
+
+func (b uiBox) Entry(labelName string) {
+	entry := ui.NewEntry()
+
+	name := ui.NewLabel(" [ "+labelName+" ] ")
+
+	hBox := ui.NewHorizontalBox()
+	hBox.SetPadded(true)
+
+	hBox.Append(name, false)
+	hBox.Append(entry, true)
+
+	b.mainVerticalBox.Append(hBox, false)
+}
+
+func(b uiBox) Erc20Info(){
+	infoEntry := ui.NewEntry()
+	infoEntry.SetReadOnly(true)
+	infoEntry.SetText("hello")
+
+	b.mainVerticalBox.Append(infoEntry, true)
 }
