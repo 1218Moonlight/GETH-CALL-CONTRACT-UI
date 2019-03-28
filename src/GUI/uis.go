52b6bb2dbde299a4617e44835d4d40ba35d1b9b3
kmoon <1218moonlight@gmail.com>
2019-03-28 17:36:52 +0900

    ca call 추가. ca call 追加。

--- src/GUI/uis.go
+++ src/GUI/uis.go
@@ -1,50 +1,101 @@
 package GUI
 
-import "github.com/andlabs/ui"
+import (
+	"github.com/andlabs/ui"
+	"fmt"
+)
 
 type uiBox struct {
-	mainVerticalBox   *ui.Box
+	mainVerticalBox *ui.Box
+	caEnter         *ui.Entry
+	eoaEnter        *ui.Entry
+}
+
+func newUiBoxManager() uiBox {
+	boxManager := uiBox{mainVerticalBox: func() *ui.Box {
+		box := ui.NewVerticalBox()
+		box.SetPadded(true)
+		return box
+	}(), caEnter: func() *ui.Entry {
+		return ui.NewEntry()
+	}(), eoaEnter: func() *ui.Entry {
+		return ui.NewEntry()
+	}()}
+	return boxManager
 }
 
 func windowUI() {
 	win := newWindow()
 	win.onClosingAndQuit()
 
-	boxManager := uiBox{mainVerticalBox:InitMainVerticalBox()}
+	///
 
-	boxManager.Entry("CA")
-	boxManager.Entry("EOA")
-	boxManager.Erc20Info()
+	boxManager := newUiBoxManager()
+
+	boxManager.caEntry()
+
+	boxManager.eoaEntry()
+
+	boxManager.erc20Info()
+
+	///
 
 	win.setChild(boxManager.mainVerticalBox)
-
 	win.show()
 }
 
-func InitMainVerticalBox()*ui.Box{
-	box := ui.NewVerticalBox()
-	box.SetPadded(true)
-	return box
-}
-
-func (b uiBox) Entry(labelName string) {
-	entry := ui.NewEntry()
-
-	name := ui.NewLabel(" [ "+labelName+" ] ")
+func (b uiBox) caEntry() {
+	name := ui.NewLabel(" [ CA ] ")
 
 	hBox := ui.NewHorizontalBox()
 	hBox.SetPadded(true)
 
 	hBox.Append(name, false)
-	hBox.Append(entry, true)
+	hBox.Append(b.caEnter, true)
 
 	b.mainVerticalBox.Append(hBox, false)
 }
 
-func(b uiBox) Erc20Info(){
+func (b uiBox) eoaEntry() {
+	name := ui.NewLabel(" [ EOA ] ")
+
+	hBox := ui.NewHorizontalBox()
+	hBox.SetPadded(true)
+
+	hBox.Append(name, false)
+	hBox.Append(b.eoaEnter, true)
+
+	b.mainVerticalBox.Append(hBox, false)
+}
+
+func (b uiBox) erc20Info() {
+	requestBtn := ui.NewButton("request")
+	b.mainVerticalBox.Append(requestBtn, false)
+
 	infoEntry := ui.NewEntry()
 	infoEntry.SetReadOnly(true)
-	infoEntry.SetText("hello")
+
+	requestBtn.OnClicked(func(button *ui.Button) {
+		if b.caEnter.Text() == "" {
+			infoEntry.SetText("Empty CA")
+			return
+		} else if b.eoaEnter.Text() == "" {
+			infoEntry.SetText("Empty EOA")
+			return
+		}
+
+		symbol, err := symbol(b.caEnter.Text())
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		balance, err := balanceOf(b.caEnter.Text(), b.eoaEnter.Text())
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		infoEntry.SetText(fmt.Sprintf("[ symbol ] : %s, [ balanceOf ] : %v", *symbol, balance))
+	})
 
 	b.mainVerticalBox.Append(infoEntry, true)
 }
